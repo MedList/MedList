2d29dd8a65269f53d1376c689a0fbadddd6b2351
OmarElFaroukSamine <omarelfarouk.samine@gmail.com>
2025-10-17 17:21:24 +0200

    Initial Commit / Setting Dependencies Using Spring Initializr

---
+++ src/main/java/com/example/MedList/MedListApplication.java
@@ -0,0 +1,13 @@
+package com.example.MedList;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MedListApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MedListApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/MedList/MedListApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.MedList;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MedListApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
